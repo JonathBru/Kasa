164a2c9cae5a679d7e53f5f55254073d93bcac8e
Bru Jonathan <jonathan.bru.marc@gmail.com>
2023-08-05 17:22:16 +0200

    Deleted react files unused

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './App.css';
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
 import HomePage from "./pages/Home";
